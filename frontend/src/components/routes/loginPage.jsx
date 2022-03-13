1c8b16f3282b2735a420d01b73e81b9f870aae56
SarahIsWeird <morningcrafter0603@gmail.com>
2022-03-13 20:15:30 +0100

    Add login and registration page

--- frontend/src/components/routes/loginPage.jsx
+++ frontend/src/components/routes/loginPage.jsx
@@ -1,29 +1,89 @@
-import {useState} from 'react';
+import { useState } from 'react';
+import { CenteredForm, ErrorParagraph, FormButton, FormField } from '../form/form';
+import { useQueryParameters } from '../referrals/hooks/useQueryParameters';
+import { buildRedirectComponentFromQueryParameters } from '../referrals/referralProcessing';
+import { PasswordInput, UsernameInput } from '../form/input';
+import { validateLoginPassword, validateUsername } from '../form/validations';
+
+const usernameErrorMessage = (
+    <ErrorParagraph>
+        Bitte gib deinen Nutzernamen ein.
+    </ErrorParagraph>
+);
+
+const passwordErrorMessage = (
+    <ErrorParagraph>
+        Bitte gib dein Passwort ein.
+    </ErrorParagraph>
+);
+
+const areAllFieldsValid = (fieldValidations) =>
+    fieldValidations.usernameValid && fieldValidations.passwordValid;
 
 export const LoginPage = () => {
     const [username, setUsername] = useState('');
+    const [isUsernameValid, setUsernameValid] = useState(true);
     const [password, setPassword] = useState('');
+    const [isPasswordValid, setPasswordValid] = useState(true);
+
+    const [redirect, setRedirect] = useState(null);
+
+    const queryParameters = useQueryParameters();
+
+    const validateFields = () => {
+        const usernameValid = validateUsername(username);
+        const passwordValid = validateLoginPassword(password);
+
+        return { usernameValid, passwordValid };
+    };
+
+    const setFieldValidations = ({ usernameValid, passwordValid }) => {
+        setUsernameValid(usernameValid);
+        setPasswordValid(passwordValid);
+    }
+
+    const redirectUser = () => {
+        const redirectComponent = buildRedirectComponentFromQueryParameters(queryParameters);
+        setRedirect(() => redirectComponent);
+    };
 
     const onSubmit = () => {
-        alert(1);
+        const fieldValidations = validateFields();
+        setFieldValidations(fieldValidations);
+
+        const allFieldsValid = areAllFieldsValid(fieldValidations);
+
+        if (allFieldsValid)
+            redirectUser();
     };
 
     return (
-        <>
-            <div className="full">
+        <CenteredForm>
+            <FormField>
                 <h2>Login</h2>
-            </div>
-            <div className="full form-field">
+                <p>Um Kommentare schreiben zu k&ouml;nnen, musst du dich einloggen.</p>
+                <p>Du hast noch keinen Account? <a href="/registrierung">Hier kannst du dich registrieren.</a></p>
+            </FormField>
+            <FormField>
                 <span>Nutzername</span><br />
-                <input type="text" value={username} onChange={event => setUsername(event.target.value)} />
-            </div>
-            <div className="full form-field">
-                <span>Password</span><br />
-                <input type="password" value={password} onChange={event => setPassword(event.target.value)} />
-            </div>
-            <div className="full form-field">
-                <button onClick={onSubmit}>Einloggen</button>
-            </div>
-        </>
+                <UsernameInput
+                    value={ username }
+                    setValue={ setUsername }
+                    isValid={ isUsernameValid } />
+                { isUsernameValid ? null : usernameErrorMessage }
+            </FormField>
+            <FormField>
+                <span>Passwort</span><br />
+                <PasswordInput
+                    value={ password }
+                    setValue={ setPassword }
+                    isValid={ isPasswordValid } />
+                { isPasswordValid ? null : passwordErrorMessage }
+            </FormField>
+            <FormField>
+                <FormButton onClick={ onSubmit }>Einloggen</FormButton>
+            </FormField>
+            { redirect }
+        </CenteredForm>
     );
 };
