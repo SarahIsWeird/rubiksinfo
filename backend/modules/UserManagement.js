c1201ad7eb428d03c8bdecd00f1aac0a44557863
Bruce Weber <bruce.weber@t2informatik.de>
2022-03-14 18:42:49 +0100

    completed User Managment

--- backend/modules/UserManagement.js
+++ backend/modules/UserManagement.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const cookieParser = require('cookie-parser');
-const { v4: uuidv4 } = require('uuid');
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const { v4: uuidv4 } = require("uuid");
 
 const app = express();
 app.use(cookieParser());
@@ -9,36 +9,113 @@ var userList = [];
 const sessions = [];
 
 function register(req) {
-    var visits = {
-        geschichte: 0,
-        arten: 0,
-        loesen: 0,
-    }
+  var visits = {
+    geschichte: 0,
+    arten: 0,
+    loesen: 0,
+  };
 
-    var userId = uuidv4();
+  var userId = uuidv4();
 
-    var user = {
-        userId: userId,
-        name: req.name,
-        passwordHash: req.passwordHash,
-        visits: visits,
-        favorites: [],
-    }
+  var user = {
+    userId: userId,
+    name: req.name,
+    passwordHash: req.passwordHash,
+    visits: visits,
+    favorites: [],
+  };
 
-    userList.push(user);
-    return userId;
+  userList.push(user);
+  return userId;
 }
 
 function login(req) {
-    return userList.find(user => user.name === req.name && user.passwordHash === req.passwordHash);
+  return userList.find(
+    (user) => user.name === req.name && user.passwordHash === req.passwordHash
+  );
 }
 
 function setCookie(res) {
-    const sessionId = uuidv4();
-    sessions.push(sessionId);
-    const cookieParameters = {
-        maxAge: 1000 * 60 * 60 * 24 * 7,
-    }
-    res.cookie("session", sessionId, cookieParameters);
+  const sessionId = uuidv4();
+  sessions.push(sessionId);
+  const cookieParameters = {
+    maxAge: 1000 * 60 * 60 * 24 * 7,
+  };
+  res.cookie("session", sessionId, cookieParameters);
 }
-module.exports = {register, login, setCookie};
+
+function addFavorite(req, res) {
+  let user = userList.find((user) => user.userId === req.userId);
+  if (!user) {
+    res.status(406);
+    res.send();
+  } else {
+    user.favorites.push(req.content);
+    console.log(user.favorites);
+  }
+}
+
+function getFavorites(userId, res) {
+  let user = userList.find((user) => user.userId === userId);
+  if (!user) {
+    res.status(406);
+    res.send();
+  } else {
+    return user.favorites;
+  }
+}
+
+function deleteFavorite(req, res) {
+  let user = userList.find((user) => user.userId === req.userId);
+  if (!user) {
+    res.status(406);
+    res.send();
+  } else {
+    let find = user.favorites.find((fav) => fav === req.content);
+    favIndex = user.favorites.indexOf(find);
+    user.favorites.splice(favIndex, 1);
+  }
+}
+
+function getMostVisitedPage(userId, res) {
+  let user = userList.find((user) => user.userId === userId);
+  if (!user) {
+    res.status(406);
+    res.send();
+  } else {
+    let maxKey;
+    let maxValue = -1;
+    for ([key, value] of Object.entries(user.visits)) {
+      if (value > maxValue) {
+        maxValue = value;
+        maxKey = key;
+      }
+    }
+    return maxKey;
+  }
+}
+
+function visitedPage(req, res) {
+  let user = userList.find((user) => user.userId === req.userId);
+  if (!user) {
+    res.status(406);
+    res.send();
+  } else {
+    for (key of Object.keys(user.visits)) {
+      if (key === req.content) {
+        user.visits[key] += 1;
+      }
+    }
+  }
+}
+
+module.exports = {
+  register,
+  login,
+  setCookie,
+  getFavorites,
+  addFavorite,
+  deleteFavorite,
+  getMostVisitedPage,
+  visitedPage,
+};
