336a770a3e8019bd66b24bb37050a051d75ab22d
jakru00 <jakru00@gmx.de>
2022-03-15 17:10:08 +0100

    changed user not found status code to 404 and introduced constant

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,8 @@ const commentManagement = require("./modules/CommentManagement");
 const app = express();
 const port = process.env.RI_BACKEND_PORT || 8080;
 
+const userNotFoundStatusCode = 404;
+
 app.listen(port, () => {
   console.log(`Listening on http://localhost:${port}/`);
 });
@@ -50,7 +52,7 @@ app.delete("/user/logout", (req, res) => {
 app.post("/user/login", jsonParser, (req, res) => {
   let user = userManagement.login(req.body);
   if (!user) {
-    res.status(406);
+    res.status(userNotFoundStatusCode);
     res.send();
   } else {
     userManagement.setCookie(res);
