eb604443c2118aa86de63e7abf8b5c4fba459b49
Marvun <knoma12@web.de>
2022-03-16 22:01:48 +0100

    Fixed dependency of UserManagement

--- backend/modules/CommentManagement.js
+++ backend/modules/CommentManagement.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const { v4: uuidv4 } = require("uuid");
 
-const userManagement = require("./modules/UserManagement");
+const userManagement = require("./UserManagement");
 
 const app = express();
 
