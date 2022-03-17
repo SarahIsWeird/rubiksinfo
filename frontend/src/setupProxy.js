37d1fdc3455ae23921bef907e5e7eff9d9835d3e
SarahIsWeird <morningcrafter0603@gmail.com>
2022-03-17 20:09:54 +0100

    Add env variable for backend url

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -2,7 +2,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = (app) => {
     app.use('/api', createProxyMiddleware({
-        target: 'http://localhost:8080',
+        target: `http://${process.env.REACT_APP_RI_BACKEND_URL || 'localhost'}`,
         changeOrigin: true,
         pathRewrite: {
             '^/api': '',
