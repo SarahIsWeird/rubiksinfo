ceeedf5813b32e238b6d9ddf1cc6962c45ce85b2
8ruz3l <91565502+8ruz3l@users.noreply.github.com>
2022-03-17 14:22:26 +0100

    Backend security (#30)
    * cant add already existing user
    
    * moved auth into own file
    
    * added Middleware functions
    
    * renamed AuthManagement to SessionManagement
    
    * cookie removal is now in SessionManagemant
    
    * added userId fetch into middleware
    
    * getting sessionId from cookies, receiving userId and setting both to req.body
    
    * removed logs
    
    Co-authored-by: Bruce Weber <bruce.weber@t2informatik.de>

--- backend/index.js
+++ backend/index.js
@@ -1,104 +1,107 @@
 const express = require("express");
 var bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 var jsonParser = bodyParser.json();
 
 const userManagement = require("./modules/UserManagement");
 const commentManagement = require("./modules/CommentManagement");
+const sessionManagement = require("./modules/SessionManagement");
 
 const app = express();
 const port = process.env.RI_BACKEND_PORT || 8080;
 
+app.use(cookieParser());
+
 const notFoundStatusCode = require("./config.json").statusCodes.notFoundCode;
+const conflictCode = require("./config.json").statusCodes.conflictCode;
+const notAuthorizedCode = require("./config.json").statusCodes.notAuthorized;
+
+function isAuthenticated(req, res, next) {
+  let userId = sessionManagement.getUserId(req.cookies.session);
+  if (userId) {
+    req.body.sessionId = req.cookies.session;
+    req.body.userId = userId;
+    next();
+  } else {
+    res.status(notAuthorizedCode);
+    res.send();
+  }
+}
+
+app.use(jsonParser);
+app.use("/user/", isAuthenticated);
+app.use("/comment/", isAuthenticated);
+app.use("/auth/logout", isAuthenticated);
 
 app.listen(port, () => {
   console.log(`Listening on http://localhost:${port}/`);
 });
 
-app.post("/user/register", jsonParser, (req, res) => {
-  let userId = userManagement.register(req.body);
-  userManagement.setCookie(res);
-  res.send({
-    userId: userId   
-  });
-});
-
-app.put("/user/favorite", jsonParser, (req, res) => {
-  let success = userManagement.addFavorite(req.body);
-  if(success) {
-    res.send();
+app.post("/auth/register", (req, res) => {
+  let userId = userManagement.addUser(req.body);
+  if (userId) {
+    let sessionId = sessionManagement.addSession(res, userId);
+    res.send({ sessionId: sessionId });
   } else {
-    res.send(notFoundStatusCode);
+    res.status(conflictCode);
+    res.send();
   }
 });
 
-app.get("/user/most-visited", (req, res) => {
-  let maxKey = userManagement.getMostVisitedPage(req.query.userId, res);
-  if(maxKey) {
+app.post("/auth/login", (req, res) => {
+  let user = userManagement.getUserByName(req.body.name);
+  if (user) {
+    const sessionId = sessionManagement.addSession(res, user.userId);
     res.send({
-      content: maxKey,
+      sessionId: sessionId,
     });
   } else {
-    res.send(notFoundStatusCode);
-  }
-});
-
-app.post("/user/most-visited", jsonParser, (req, res) => {
-  let success = userManagement.visitedPage(req.body, res);
-  if(success) {
+    res.status(notFoundStatusCode);
     res.send();
-  } else {
-    res.send(notFoundStatusCode);
   }
 });
 
-app.get("/user/favorite", (req, res) => {
-  let favorites = userManagement.getFavorites(req.query.userId, res);
-  if(favorites){
-    res.send({
-      favorites: favorites,
-    });
-  } else {
-    res.send(notFoundStatusCode);
-  }
-});
-
-app.delete("/user/favorite", jsonParser, (req, res) => {
-  let success = userManagement.deleteFavorite(req.body, res);
-  if(success) {
-    res.send();
-  } else 
-  res.send(notFoundStatusCode);
-});
-
-app.delete("/user/logout", (req, res) => {
-  res.clearCookie("session");
+app.delete("/auth/logout", (req, res) => {
+  sessionManagement.removeSession(req.body.sessionId, res);
   res.send();
 });
 
-app.post("/user/login", jsonParser, (req, res) => {
-  let user = userManagement.login(req.body);
-  if (!user) {
-    res.send(notFoundStatusCode);
-  } else {
-    userManagement.setCookie(res);
-    res.send({
-        userId: user.userId,
-    });
-  }
+app.post("/user/favorite", (req, res) => {
+  userManagement.addFavorite(req.body, res);
+  res.send();
 });
 
-app.post("/comment", jsonParser, (req, res) => {
-  commentManagement.addComment(req.body);
+app.get("/user/most-visited", (req, res) => {
+  res.send({
+    "most-visited": userManagement.getMostVisitedPage(req.body.userId, res),
+  });
+});
+
+app.post("/user/most-visited", (req, res) => {
+  userManagement.visitedPage(req.body, res);
+  res.send();
+});
+
+app.get("/user/favorite", (req, res) => {
+  let favorites = userManagement.getFavorites(req.body.userId, res);
+  res.send({
+    favorites: favorites,
+  });
+});
+
+app.delete("/user/favorite", (req, res) => {
+  userManagement.removeFavorite(req.body, res);
+  res.send();
+});
+
+app.post("/comment", (req, res) => {
+  let user = userManagement.getUserById(req.body.userId);
+  commentManagement.addComment(req.body, user.name);
   res.send();
 });
 
 app.get("/comment", (req, res) => {
-  let comments = commentManagement.getComments(req.query.origin);
-  if(comments) {
-    res.send({
-      comments: comments
-    });
-  } else {
-    res.send(notFoundStatusCode);
-  }
+  res.send({
+    comments: commentManagement.getComments(req.query.origin),
+  });
 });
---
+++ backend/modules/SessionManagement.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const { v4: uuidv4 } = require("uuid");
+
+const app = express();
+app.use(cookieParser());
+
+const sessions = [];
+
+function addSession(res, userId) {
+  const sessionId = uuidv4();
+  sessions.push({ sessionId: sessionId, userId: userId });
+  const cookieParameters = {
+    maxAge: 1000 * 60 * 60 * 24 * 7,
+  };
+  res.cookie("session", sessionId, cookieParameters);
+  return sessionId;
+}
+
+function removeSession(sessionId, res) {
+  let sessionIndex = sessions.findIndex(
+    (session) => session.sessionId === sessionId
+  );
+  sessions.splice(sessionIndex, 1);
+  res.clearCookie("session");
+}
+
+function getUserId(sessionId) {
+  const session = sessions.find((session) => session.sessionId === sessionId);
+  return session ? session.userId : undefined;
+}
+
+module.exports = {
+  addSession,
+  removeSession,
+  getUserId,
+};
