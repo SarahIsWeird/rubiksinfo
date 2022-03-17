ace71d55b4dc6134417d4c1215f944f806256cd1
SarahIsWeird <morningcrafter0603@gmail.com>
2022-03-17 20:36:38 +0100

    Fix env 2

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -2,7 +2,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = (app) => {
     app.use('/api', createProxyMiddleware({
-        target: process.env.REACT_APP_RI_BACKEND_URL || 'localhost',
+        target: process.env.REACT_APP_RI_BACKEND_URL || 'http://localhost:8080',
         changeOrigin: true,
         pathRewrite: {
             '^/api': '',
